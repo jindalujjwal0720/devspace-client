844b7ffeb25693248621e80b85ee3baf3daaf018
Ujjwal Jindal <jindalujjwal0720@gmail.com>
2023-08-28 17:45:30 +0530

    initial commit - setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+/*
+ * Devspace IIT(ISM) Dhanbad
+ *
+ * This software and related documentation are owned by Devspace IIT(ISM) Dhanbad.
+ * Unauthorized copying, reproduction, or modification via any medium is strictly prohibited.
+ * Proprietary and confidential. All rights reserved.
+ */
+
+// import logo from './logo.svg';
+import "./App.css";
+
+function App() {
+  return <div className="App">Devspace - Central App</div>;
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*
+ * Devspace IIT(ISM) Dhanbad
+ *
+ * This software and related documentation are owned by Devspace IIT(ISM) Dhanbad.
+ * Unauthorized copying, reproduction, or modification via any medium is strictly prohibited.
+ * Proprietary and confidential. All rights reserved.
+ */
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+// import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
@@ -14,4 +22,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
